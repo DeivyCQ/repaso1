2ed4149dbcdbf1b298b9282b850d433aafbf7615
saulbarrios <barriossaul@gmail.com>
2020-12-12 00:31:40 -0500

    actualizacion migraciones

--- pachaqtec/admin.py
+++ pachaqtec/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Categoria
+from .models import Categoria, Cursos, Alumno, Cupon
 
 # Register your models here.
 admin.site.register(Categoria)
+admin.site.register(Cursos)
+admin.site.register(Alumno)
+admin.site.register(Cupon)
